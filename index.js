08f5917db8895781e51aea8952201b677b895f4e
Antonio Harris Jr <your_ajharris0201@gmail.com>
2026-06-28 12:50:32 -0400

    puppybowl changes

--- index.js
+++ index.js
@@ -5,7 +5,15 @@
 
 /////////////////////////////
 /*This looks like a good place to declare any state or global variables you might need*/
-
+let players = [];
+let singlePlayer = null;
+let teams = [];
+let selectedPlayerId = null;
+const playersInBowl = document.querySelector("#listOfPlayers");
+const singlePlayerDiv = document.querySelector("#details");
+const selectPlayerDiv = document.querySelector("#selectPlayer");
+const puppyForm = document.querySelector("#puppyForm");
+const teamSelect = document.querySelector("#teamSelect");
 ////////////////////////////
 
 /**
@@ -14,7 +22,47 @@
  * Instead, this function should be keeping our state up to date
  */
 const fetchAllPlayers = async () => {
-  //TODO
+  try {
+    const response = await fetch(
+      "https://fsa-puppy-bowl.herokuapp.com/api/2605-Antonio/players",
+    );
+    const result = await response.json();
+    players = result.data.players;
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+playersInBowl.addEventListener("click", async (event) => {
+  if (event.target.classList.contains("playerName")) {
+    selectedPlayerId = event.target.dataset.playerid;
+
+    const previousHighlight = document.querySelector(".playerName.highlighted");
+
+    if (previousHighlight) {
+      previousHighlight.classList.remove("highlighted");
+    }
+
+    event.target.classList.add("highlighted");
+
+    await fetchSinglePlayer(selectedPlayerId);
+    console.log(singlePlayer);
+    render();
+  }
+});
+
+const fetchAllTeams = async () => {
+  try {
+    const response = await fetch(
+      "https://fsa-puppy-bowl.herokuapp.com/api/2605-Antonio/teams",
+    );
+    const result = await response.json();
+    teams = result.data.teams;
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 /**
@@ -28,7 +76,15 @@ const fetchAllPlayers = async () => {
  * Unless we know the id of the player we are trying to fetch, we cannot call fetchSinglePlayer()
  */
 const fetchSinglePlayer = async (playerId) => {
-  //TODO
+  try {
+    const response = await fetch(
+      `https://fsa-puppy-bowl.herokuapp.com/api/2605-Antonio/players/${playerId}`,
+    );
+    const { data } = await response.json();
+    singlePlayer = data.player;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 /**
@@ -47,8 +103,45 @@ const fetchSinglePlayer = async (playerId) => {
  */
 
 const addNewPlayer = async (newPlayer) => {
-  //TODO
+  try {
+    const response = await fetch(
+      "https://fsa-puppy-bowl.herokuapp.com/api/2605-Antonio/players/",
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(newPlayer),
+      },
+    );
+    const result = await response.json();
+    if (!result.success) {
+      console.log(result.error);
+      return;
+    }
+    console.log(response.status);
+    console.log(result);
+    players.push(result.data.newPlayer);
+  } catch (error) {
+    console.log(error);
+  }
 };
+puppyForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const formData = new FormData(puppyForm);
+  const newPlayer = {
+    name: formData.get("pName"),
+    breed: formData.get("breed"),
+    status: formData.get("status"),
+    imageUrl: formData.get("imageUrl"),
+    teamId: formData.get("teamId"),
+  };
+  console.log("New Player:", newPlayer);
+  await addNewPlayer(newPlayer);
+  console.log(newPlayer);
+
+  render();
+});
 
 /**
  * Removes a player from the roster via the API.
@@ -62,9 +155,27 @@ const addNewPlayer = async (newPlayer) => {
  */
 
 const removePlayer = async (playerId) => {
-  //TODO
+  try {
+    const response = await fetch(
+      `https://fsa-puppy-bowl.herokuapp.com/api/2605-Antonio/players/${playerId}`,
+      {
+        method: "DELETE",
+      },
+    );
+    players = players.filter((player) => {
+      return Number(player.id) !== Number(playerId);
+    });
+    singlePlayer = null;
+  } catch (error) {
+    console.log(error);
+  }
 };
-
+singlePlayerDiv.addEventListener("click", async (event) => {
+  if (event.target.classList.contains("deleteButton")) {
+    await removePlayer(event.target.dataset.singleplayerid);
+    render();
+  }
+});
 /**
  * Updates html to display a list of all players or a single player page.
  *
@@ -82,7 +193,41 @@ const removePlayer = async (playerId) => {
  *
  */
 const render = () => {
-  // TODO
+  const playerHTML = players.map((player) => {
+    return `
+    <div class="listOfPlayers"> 
+     <img class="photo" src="${player.imageUrl}"alt="${player.name}"/>
+    <h2 class="playerName" data-playerId=${player.id}>${player.name}</h2>
+    </div>
+    `;
+  });
+  playersInBowl.innerHTML = playerHTML.join("");
+
+  const teamHTML = teams.map((team) => {
+    return `
+    <option value=${team.id}> ${team.name}</option>
+    `;
+  });
+  teamSelect.innerHTML =
+    `<option value="">Select a Team</option>` + teamHTML.join("");
+
+  if (!singlePlayer) {
+    selectPlayerDiv.innerHTML = "Please Select Player Name to View Details.";
+    singlePlayerDiv.innerHTML = "";
+  } else {
+    selectPlayerDiv.innerHTML = "";
+    singlePlayerDiv.innerHTML = `
+  <div>
+  <h2 class="playerName"><strong>Name:</strong> ${singlePlayer.name}</h2>
+  <h2 class="playerId"><strong>Id:</strong>  ${singlePlayer.id}</h2>
+  <h2 class="playerBreed"><strong>Breed:</strong> ${singlePlayer.breed}</h2>
+  <h2 class="playerStatus"><strong>Status:</strong> ${singlePlayer.status}</h2>
+  <img class="playerPhoto" src=${singlePlayer.imageUrl || ""} alt=${singlePlayer.name}/>
+  <h2 class="playerTeamName"><strong>Team:</strong>  ${singlePlayer.team ? singlePlayer.team.name : "Unassigned"}</h2>
+  <br />
+  <button class="deleteButton" data-singlePlayerId=${singlePlayer.id}>Delete</button>
+  </div>`;
+  }
 };
 
 /**
@@ -90,8 +235,8 @@ const render = () => {
  * HOWEVER....
  */
 const init = async () => {
-  //Before we render, what do we always need?
-
+  await fetchAllPlayers();
+  await fetchAllTeams();
   render();
 };
 
